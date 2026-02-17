c9040cdf1a8de11e344fd844272a7257760bd765
Shubhek Kumar <123364197+shubhekkumar@users.noreply.github.com>
2026-02-18 00:36:03 +0530

    Refactor docker_build function to include ProjectName

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,2 +1,5 @@
-def call(String ImageTag, String DockerHubUser){
-sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
+def call(String ProjectName, String ImageTag, String DockerHubUser) {
+    sh """
+        docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} .
+    """
+}
